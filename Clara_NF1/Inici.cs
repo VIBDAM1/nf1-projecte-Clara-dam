718968feb548d2dd5d89b855eb24ae8ac502ddf8
clara <clpe085@vidalibarraquer.net>
2025-02-23 00:17:46 +0100

    interactive version

--- Clara_NF1/Inici.cs
+++ Clara_NF1/Inici.cs
@@ -7,12 +7,12 @@ namespace Clara_NF1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hola");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
